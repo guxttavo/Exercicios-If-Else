91c36cf379ca98e172fbc99d5cdf302615708d72
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 11:14:54 -0300

    ex 03

--- src/Ex03.java
+++ src/Ex03.java
@@ -1,5 +1,6 @@
 /*
-
+-->Faça um programa que verifique se uma letra digitada é "F" ou "M".
+-->Conforme a letra escrever: F - Feminino, M - Masculinho, Sexo inválido.
 
  */
 
@@ -9,5 +10,16 @@ import java.util.Scanner;
 public class Ex03 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com uma letra(F ou M)");
+        String input = sc.next();
+
+        if (input.equalsIgnoreCase("f")) {
+            System.out.println("F- Feminino");
+        } else if (input.equalsIgnoreCase("m")) {
+            System.out.println("M - Masculino");
+        } else {
+            System.out.println("Sexo invalido");
+        }
     }
 }
