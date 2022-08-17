6b1a55144dfd1c5549e3019975b5dbc6ff1c41f7
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 10:51:47 -0300

    ex 02

--- src/Ex02.java
+++ src/Ex02.java
@@ -1,7 +1,23 @@
+/*
+-->Faça um programa que peça um valor e mostre na tela se o valor é positivo ou negativo
+
+*/
+
 import java.util.Scanner;
 
 public class Ex02 {
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
+
+
+        System.out.println("Digite um numero: ");
+        int num = sc.nextInt();
+
+        if (num >= 0) {
+            System.out.println("Numero positivo");
+        } else {
+            System.out.println("Numero negativo");
+        }
     }
 }
