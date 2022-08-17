9ec2e4bb96b5bfd8de7190205f5e6aa102aae6c6
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 14:50:32 -0300

    ex 09

--- src/Ex09.java
+++ src/Ex09.java
@@ -1,6 +1,7 @@
 /*
-
-
+-->Faça um programa que pergunte em que turno você estuda.
+-->Peça para digitar M-Matutino ou V-Vespertino ou N-Noturno.
+-->Imprima a mensagem "Bom dia!", "Boa tarde!", ou "Boa noite!" ou "Valor Inválido!", conforme o caso.
  */
 
 import java.util.Scanner;
@@ -8,5 +9,26 @@ import java.util.Scanner;
 public class Ex09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Em qual turno voce estuda(M-Matutino ou V-Vespertino ou N-Noturno)?");
+        String turno = sc.next();
+
+        switch (turno) {
+            case "m":
+            case "M":
+                System.out.println("Bom dia!");
+                break;
+            case "v":
+            case "V":
+                System.out.println("Boa tarde!");
+                break;
+            case "n":
+            case "N":
+                System.out.println("Boa noite!");
+                break;
+            default:
+                System.out.println("Valor inválido!");
+
+        }
     }
 }
