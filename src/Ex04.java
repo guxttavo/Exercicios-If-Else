cfcc61f39b033cf231ea4f57aa6f6fed1448f34d
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 11:26:37 -0300

    ex 04

--- src/Ex04.java
+++ src/Ex04.java
@@ -1,5 +1,5 @@
 /*
-
+-->Faça um Programa que verifique se uma letra digitada é vogal ou consoante.
 
  */
 
@@ -8,5 +8,28 @@ import java.util.Scanner;
 public class Ex04 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite uma letra: ");
+        String input = sc.next();
+
+
+        if (input.length() > 1) {
+            System.out.println("Invalido, digite apenas uma letra!");
+        }else{
+            switch (input) {
+                case "a":
+                case "e":
+                case "i":
+                case "o":
+                case "u":
+                    System.out.println("Vogal");
+                    break;
+                default:
+                    System.out.println("Consoante");
+
+            }
+
+        }
+
     }
 }
