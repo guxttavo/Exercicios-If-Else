b71bf00d83d7e2f89c7f7b33bef66faa9d36320f
GusttavoBarbosa <gustavowbarbosas@gmail.com>
2022-08-17 11:48:58 -0300

    ex 06

--- src/Ex06.java
+++ src/Ex06.java
@@ -1,6 +1,6 @@
 
 /*
-
+-->Faça um programa que leia três números e mostre o maior deles.
 
  */
 
@@ -9,5 +9,18 @@ import java.util.Scanner;
 public class Ex06 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        System.out.println("Digite tres numeros: ");
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        int num3 = sc.nextInt();
+
+        if (num1 > num2 && num1 > num3) {
+            System.out.println("Primeiro numero eh o maior");
+        } else if (num2 > num1 && num2 > num3) {
+            System.out.println("Segundo numero eh o maior");
+        } else if (num3 > num1 && num3 > num2) {
+            System.out.println("Terceiro numero eh o maior");
+        }
     }
 }
